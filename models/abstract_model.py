a1e3865008fef3e3af822fddc6427c022d9e5323
SimonyanHayk02 <hayk.simonyan.2002@gmail.com>
2024-02-16 10:52:09 +0400

    some changes in abstract model

--- models/abstract_model.py
+++ models/abstract_model.py
@@ -1,6 +1,7 @@
 # Model Base Class
 from abc import ABC, abstractmethod
 from pathlib import Path
+import pickle
 from helper import logger
 
 class Model(ABC):
@@ -22,11 +23,27 @@ class Model(ABC):
 
     @abstractmethod
     def save(self, path=None):
-        pass
+        if path is None:
+            path = f"{self.name}.pkl"
+
+        try:
+            with open(path, 'wb') as file:
+                pickle.dump(self, file)
+            logger.info(f"Model saved to {path}")
+        except Exception as e:
+            logger.error(f"Failed to save the model: {e}")
+        
 
     @abstractmethod
     def load(self, path=None):
-        pass
+        try:
+            with open(path, 'rb') as file:
+                model = pickle.load(file)
+            logger.info(f"Model loaded from {path}")
+            return model
+        except Exception as e:
+            logger.error(f"Failed to load the model: {e}")
+            return None
 
     def __str__(self):
         return self.name
